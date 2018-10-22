668d20561071e660c4121919888d30f2c1746dc8
Jean-Christophe Maigrot <jean-christophe.maigrot@advisorsla.com>
2018-10-22 23:21:39 +0200

    Create Bookmark json types.

--- src/bookmark-table/bookmark.type.ts
+++ src/bookmark-table/bookmark.type.ts
@@ -16,3 +16,17 @@ export interface IBookmark {
 }
 
 export type TBookmarkList = IBookmark[];
+
+export interface IBookmarkJSON {
+  authorName: string;
+  creationDate: string;
+  duration: number | null;
+  height: number;
+  id: number;
+  title: string;
+  type: EType;
+  url: string;
+  width: number;
+}
+
+export type TBookmarkJSONList = IBookmarkJSON[];
