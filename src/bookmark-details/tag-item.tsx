3cdab6956bbadf8fe3a75193d934ec9e98a0759a
Jean-Christophe Maigrot <jean-christophe.maigrot@advisorsla.com>
2018-11-03 14:57:16 +0100

    Create TagItem Component

---
+++ src/bookmark-details/tag-item.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+
+import { ITag } from '../types/tag.type';
+
+export interface ITagItemProps {
+  tag: ITag;
+  onTagRemove: (tagId: number) => () => void;
+}
+
+export const TagItem = (props: ITagItemProps) => (
+  <li id={`tag-${props.tag.name}`}>
+    <span>{props.tag.name}</span>
+    <DeleteForeverIcon
+      id={`delete-bookmark-${props.tag.id}`}
+      onClick={props.onTagRemove(props.tag.id)}
+    />
+  </li>
+);
