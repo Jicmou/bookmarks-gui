cbee09c92ef7948c08cf92185c866dfc2cc8821e
Jean-Christophe Maigrot <jean-christophe.maigrot@advisorsla.com>
2018-11-04 12:35:18 +0100

    Reorder alphabetically

--- src/services/bookmark.service.type.ts
+++ src/services/bookmark.service.type.ts
@@ -5,29 +5,29 @@ export interface IBookmarkService {
   addTagToBookmarkTagList: (
     bookmark: bookmarkTypes.IBookmarkWithTagList,
   ) => (tagName: string) => bookmarkTypes.IBookmarkWithTagList;
-  fromJSONToBookmark: (
-    bookmarkJSON: bookmarkTypes.IBookmarkJSON,
-  ) => bookmarkTypes.IBookmark;
-  getBookmarkList: (
-    fetch: Fetch,
-  ) => (apiUrl: string) => Promise<bookmarkTypes.TBookmarkList>;
   createBookmark: (
     fetch: Fetch,
   ) => (apiUrl: string) => (link: string) => Promise<bookmarkTypes.IBookmark>;
   deleteBookmark: (
     fetch: Fetch,
   ) => (apiUrl: string) => (id: number) => Promise<IResponse>;
+  fromJSONToBookmark: (
+    bookmarkJSON: bookmarkTypes.IBookmarkJSON,
+  ) => bookmarkTypes.IBookmark;
+  getBookmarkDetailsWithTagList: (
+    fetch: Fetch,
+  ) => (
+    apiUrl: string,
+  ) => (id: number) => Promise<bookmarkTypes.IBookmarkWithTagList>;
+  getBookmarkList: (
+    fetch: Fetch,
+  ) => (apiUrl: string) => Promise<bookmarkTypes.TBookmarkList>;
   removeBookmarkFromList: (
     bookmarkList: bookmarkTypes.TBookmarkList,
   ) => (bookmarkId: number) => bookmarkTypes.TBookmarkList;
   removeTagFromBookmarkById: (
     bookmark: bookmarkTypes.IBookmarkWithTagList,
   ) => (tagId: number) => bookmarkTypes.IBookmarkWithTagList;
-  getBookmarkDetailsWithTagList: (
-    fetch: Fetch,
-  ) => (
-    apiUrl: string,
-  ) => (id: number) => Promise<bookmarkTypes.IBookmarkWithTagList>;
 }
 
 export interface IServerErrorMessage {
